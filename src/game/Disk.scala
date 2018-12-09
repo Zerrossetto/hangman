5f1e2c1a75de34f86cab4e48637ce5fccdc69b94
Duilio Rumbazzi <Zerrossetto@users.noreply.github.com>
2018-12-09 15:58:17 +0100

    Disk class

--- src/game/Disk.scala
+++ src/game/Disk.scala
@@ -1,27 +1,43 @@
 package game
 
+import scala.collection.mutable.ListBuffer
+
 /** Handles read/write from/to Disk.
   * Reading uses java.util.Scanner
   * Writing uses java.io
   */
 object Disk {
 
-  def loadAllResults() = ???
 
-  def appendResult(playerName: String, points: Int, fileName: String) = {
-    val fil = new java.io.File(fileName)
-    val scannad = new java.util.Scanner(fil)
-    val nyRad: String = ("Namn: " + playerName + "\n" + "Poäng: " + points.toString)
-
-    val ändrad = scannad.toString + "\n" + nyRad
-    saveString(ändrad, fileName)
-
+  def loadAllResults(fileName: String) : Vector[(String, Int)] = {
+    // from mutable to immutable
+    return loadAllResultsMutable(fileName).toVector
   }
 
-  /** Save `text` to a text file called `fileName`. */
-  private def saveString(text: String, fileName: String): Unit = {
-    val f = new java.io.File(fileName)
-    val pw = new java.io.PrintWriter(f, "UTF-8")
-    try pw.write(text) finally pw.close()
+  private def loadAllResultsMutable(fileName: String) : ListBuffer[(String, Int)] = {
+
+    val file = new java.io.InputStreamReader(
+      new java.io.FileInputStream(fileName),
+      java.nio.charset.Charset.forName("UTF-8").newDecoder()
+    )
+    val scanner = new java.util.Scanner(file)
+    var scores = new ListBuffer[(String, Int)]()
+
+    while (scanner.hasNextLine()) {
+      val scoreLine = scanner.nextLine().split(';')
+      scores += new Tuple(scoreLine(0), scoreLine(1).toInt)
+    }
+    file.close()
+
+    return scores
+  }
+
+  def appendResult(fileName: String, playerName: String, points: Int) = {
+    val file = new java.io.OutputStreamWriter(
+      new java.io.FileOutputStream(fileName),
+      java.nio.charset.Charset.forName("UTF-8").newEncoder()
+    )
+    file.append(playerName + ";" + points)
+    file.close()
   }
 }
